a6c6badf61d12b1a8d2d786789be7de65881bd1a
apdouziz1 <apdiasiisipraahim118gmail.com>
2024-09-05 10:22:50 +0300

    modified todo app

--- main.js
+++ main.js
@@ -3,6 +3,9 @@
 function addTodo(){
     const inputElement=document.querySelector('input');
     const value=inputElement.value;
+    if(value===''){
+        return;
+    }
 
     const Todocontainer=document.createElement('div');
     Todocontainer.className='flex justify-between items-center';
@@ -13,6 +16,7 @@ function addTodo(){
     const doneButton=document.createElement('button');
     doneButton.textContent='Done';
     doneButton.className='bg-green-500 p-2 rounded-lg';
+    doneButton.onclick=MarkAsDone;
 
     Todocontainer.appendChild(todoTitle);
     Todocontainer.appendChild(doneButton);
@@ -22,5 +26,39 @@ function addTodo(){
 
     inputElement.value='';
     
+    
+
+}
+
+
+function MarkAsDone(event){
+const clickButton=event.target;
+clickButton.classList.add('hidden');
+
+
+
+const todoItem=clickButton.previousElementSibling;
+todoItem.classList.add('line-through');
+
+clickButton.textContent='Delete';
+clickButton.className='bg-red-500 p-2 rounded-lg shadow-lg';
+clickButton.onclick=deletebtn;
+// clickButton.classList.add('hidden');
+
+
+
+
+
+
+
+}
+
+
+function deletebtn(event){
+    const deletebtn=event.target;
+    
+    const deletetodo=deletebtn.previousElementSibling;
+    deletetodo.remove();
+    deletebtn.remove();
 
 }
